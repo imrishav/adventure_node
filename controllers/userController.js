ede8b9a1f5c5d29e681a3a24f294805dd756c0dc
Rishav Sinha <rishav@live.co.uk>
2020-04-19 04:38:32 +0530

    Intitalized the project

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This Route is Not Yet defined',
+  });
+};
+exports.createUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This Route is Not Yet defined',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This Route is Not Yet defined',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This Route is Not Yet defined',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This Route is Not Yet defined',
+  });
+};
